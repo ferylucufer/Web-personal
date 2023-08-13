478642b99029603d0be30ad25ca99a3ba7291db8
ferylucufer <56514878+ferylucufer@users.noreply.github.com>
2023-08-14 01:05:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".eka", {
+  strings: [
+    "<i>Frontend Developer",
+    "<i>Youtuber",
+    "<i>Gamers",
+    "<i>Blokchain",
+    "<i>Kuli Bangunan",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
